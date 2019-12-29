f6bf6863e842a3522a345b89234921012f099a49
BergadiAymen <57805175+BergadiAymen@users.noreply.github.com>
2019-12-29 14:52:16 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,27 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*'uses'=>'logController@connecter',
-    'as' => 'login.redirect'*/
 
-//route du debut
 Route::get('/', function () {
-    return view('login');
+    return view('welcome');
 });
-
-Route::post('/connecter','logController@connecter');
-/*Route::post('/connecter', function (){
-    echo "it works";
-});*/
-Route::post('/ajouter','EtudController@ajouteretud');
-
-Route::post('/rediriger',[
-    'uses'=>'logController@connecter',
-    'as' => 'login.redirect'
-]);
-
-//route 02 : home pour rediriger admin quand il se connecte, vers ajout des etudiants et pouvoir voir la liste des etudiants
-
-Route::get('/home', function () {
-    return view('index');
-})->name('home');
