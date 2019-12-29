7e8f5ef8cb37dbd88771f9b2fcc19b9c8f0c7673
BergadiAymen <57805175+BergadiAymen@users.noreply.github.com>
2019-12-29 14:50:32 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/login','UserController@Login');
+Route::post('/etudiant/add','EtudiantController@add');
+Route::post('/enseignent/add','EnseignentController@add');
