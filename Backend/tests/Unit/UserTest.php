702ed70cacb07ac1aac22baa71fe4a3a2d69701c
Latreche Cherif <57527894+CherifMZ@users.noreply.github.com>
2020-01-02 17:39:36 +0000

    for documentation

--- Backend/tests/Unit/UserTest.php
+++ Backend/tests/Unit/UserTest.php
@@ -6,9 +6,15 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+///Classe UsertTest
+///
+/// contient la fonction du test unitaire du login
 class UserTest extends TestCase
 {
-    
+    /** 
+     * test qui verifie que l'utilisateur est l'admin 
+     * @test 
+    */
     public function testUser() {
       $this->assertDatabaseHas('utilisateur',[
           'nom' => "admin",
