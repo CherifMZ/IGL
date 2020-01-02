a4a31471cb9991232d033148b33509c2cb3b1468
Latreche Cherif <57527894+CherifMZ@users.noreply.github.com>
2020-01-02 17:40:55 +0000

    for documentation

--- Backend/app/Enseignent.php
+++ Backend/app/Enseignent.php
@@ -4,6 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/// Classe Enseignant
+///
+/// Classe qui represente le model enseignant dans la base de 
 class Enseignent extends Model
 {
     protected $table="enseignent";
